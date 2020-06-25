cc03c61f4fb09778698c11d1cfb7bdda51b4edd0
Kaspar Poland <kaspar78@mouco.com>
2020-06-25 15:58:21 -0600

    Github authentication and Socket.io integration

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# The code for Kaspar Poland's personal website, found at kasparpoland.com
+### The code for Kaspar Poland's personal website, found at kasparpoland.com
---
+++ src/server/routes.js
@@ -0,0 +1,61 @@
+import childProcess from "child_process";
+import express from "express";
+
+import Update from "./dataModels/Update.js";
+import pollGithubAndSave from "./lib.js";
+
+// ------------------
+//     ALL ROUTES
+// ------------------
+
+const router = express.Router();
+
+// Gets updates when the 'recent updates' page is visited
+router.get("/updates", async (req, res) => {
+  const updates = await Update.find()
+    .sort({ date: -1 })
+    .then(updates => updates);
+
+  return res.json(updates);
+});
+
+// For the Reed-Solomon encoder/decoder in projects
+router.post("/RS", async (req, res) => {
+  // Run the java program
+  childProcess.exec(
+    "cd src/assets/code && java qrcode/Main " + req.body.message,
+    (err, stdout, stderr) => {
+      if (err) console.log(err);
+      if (stderr) console.log(stderr.toString());
+      const dataArray = stdout.split("\n").slice(0, -1);
+      // Return the data back to the page that wanted it
+      return res.send(dataArray);
+    }
+  );
+});
+
+// -----------------
+//    TEST ROUTES
+// -----------------
+
+// Fetches immediately for testing
+router.post("/test-fetchGithub", async (req, res) => {
+  await pollGithubAndSave();
+  return res.send({ success: "Successfully fetched data from Github!" });
+});
+
+// Test GET method
+router.get("/test", (req, res) => {
+  return res.json({
+    success: "This GET test returned successfully!!"
+  });
+});
+
+// Test POST method
+router.post("/test", (req, res) => {
+  return res.json({
+    success: "This POST test returned successfully!!"
+  });
+});
+
+export default router;
