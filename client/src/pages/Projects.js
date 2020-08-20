cf749478d736d45400a3efd93f9b717b9426247e
Kaspar Poland <kaspar78@mouco.com>
2020-08-20 11:54:14 -0600

    Move stateful checking up a layer from ProjectBar

--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -2,33 +2,50 @@ import React from "react";
 import _ from "lodash";
 import TitleBar from "../components/TitleBar";
 import Grid from "@material-ui/core/Grid";
+
 import ProjectBar from "../components/ProjectBar";
 import { projects } from "../lib";
 
 import "../assets/css/projects.css";
 
-function Projects(props) {
-  return (
-    <div>
-      <TitleBar title="projects" />
-      <Grid
-        container
-        direction="column"
-        justify="center"
-        alignItems="center"
-        className="project-bars-wrapper"
-      >
-        {_.map(projects, project => (
-          <ProjectBar
-            image={project.image}
-            title={project.title}
-            path={project.path}
-            blurb={project.blurb}
-          />
-        ))}
-      </Grid>
-    </div>
-  );
+class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { width: window.outerWidth };
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", () =>
+      this.setState({ width: window.outerWidth })
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <TitleBar title="projects" />
+        <Grid
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+          className="project-bars-wrapper"
+        >
+          {_.map(projects, (project, projectName) => (
+            <ProjectBar
+              image={project.image}
+              title={project.title}
+              path={project.path}
+              blurb={project.blurb}
+              width={this.state.width}
+              isFirst={Object.keys(projects).indexOf(projectName) === 0}
+            />
+          ))}
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Projects;
