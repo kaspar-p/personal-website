2ec51ea4adc95f7a378fc48ff32fa7557b6a1bc2
Kaspar Poland <kaspar78@mouco.com>
2020-08-20 11:54:32 -0600

    Remove double lines between projects

--- client/src/components/ProjectBar.js
+++ client/src/components/ProjectBar.js
@@ -1,67 +1,61 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Grid from "@material-ui/core/Grid";
 
 import "../assets/css/project-bar.css";
 
-class ProjectBar extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      width: window.outerWidth
-    };
-  }
-
-  componentDidMount() {
-    window.addEventListener("resize", () =>
-      this.setState({ width: window.outerWidth })
-    );
-  }
-
-  render() {
-    return (
+function ProjectBar(props) {
+  return (
+    <Grid
+      item
+      container
+      direction="row"
+      justify="space-evenly"
+      alignItems="center"
+      className="project-bar"
+      style={{ marginBottom: "2rem" }}
+      lg={8}
+      xs={11}
+      onClick={() => (window.location = props.path)}
+      style={{ borderTop: props.isFirst ? "black solid 2px" : "none" }}
+    >
+      <img
+        item="true"
+        className="project-img"
+        height="90%"
+        width={props.width > 960 ? "auto" : "100%"}
+        alt={props.title}
+        src={props.image}
+        style={{ marginBottom: "2rem" }}
+      />
       <Grid
         item
         container
-        direction="row"
-        justify="space-evenly"
-        alignItems="center"
-        className="project-bar"
-        style={{ marginBottom: "2rem" }}
-        lg={8}
-        xs={11}
-        onClick={() => (window.location = this.props.path)}
+        direction="column"
+        justify="flex-start"
+        alignItems="flex-start"
+        xs={6}
+        className="project-text-section"
       >
-        <img
-          item="true"
-          className="project-img"
-          height="90%"
-          width={this.state.width > 960 ? "auto" : "100%"}
-          alt={this.props.title}
-          src={this.props.image}
-          style={{ marginBottom: "2rem" }}
-        />
-        <Grid
-          item
-          container
-          direction="column"
-          justify="flex-start"
-          alignItems="flex-start"
-          xs={6}
-          className="project-text-section"
-        >
-          <div className="project-title">
-            <h2>{this.props.title}</h2>
+        <div className="project-title">
+          <h2>{props.title}</h2>
+        </div>
+        {props.width > 960 ? (
+          <div className="project-blurb">
+            <p>{props.blurb}</p>
           </div>
-          {this.state.width > 960 ? (
-            <div className="project-blurb">
-              <p>{this.props.blurb}</p>
-            </div>
-          ) : null}
-        </Grid>
+        ) : null}
       </Grid>
-    );
-  }
+    </Grid>
+  );
 }
+
+ProjectBar.propTypes = {
+  isFirst: PropTypes.bool.isRequired,
+  title: PropTypes.string.isRequired,
+  blurb: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired
+};
 
 export default ProjectBar;
