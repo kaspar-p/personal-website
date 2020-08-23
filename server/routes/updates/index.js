45066afcbf3f4f1584e1cb294ce86fa30fa5fbee
Kaspar Poland <kaspar78@mouco.com>
2020-08-23 13:27:20 -0600

    Reallocate routes folders and make balance persist

---
+++ server/routes/updates/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+
+import Update from "../../dataModels/Update.js";
+
+const router = express.Router();
+
+// -------------------
+//    UPDATE ROUTES
+// -------------------
+
+// Gets updates when the 'recent updates' page is visited
+router.get("/", async (req, res) => {
+  const updates = await Update.find()
+    .sort({ date: -1 })
+    .then(updates => updates);
+
+  return res.json(updates);
+});
+
+// -----------------
+//    TEST ROUTES
+// -----------------
+
+// Test GET method
+router.get("/test", (req, res) => {
+  return res.json({
+    path: "/api/updates/test",
+    method: "GET",
+    status: "SUCCESS"
+  });
+});
+
+// Test POST method
+router.post("/test", (req, res) => {
+  return res.json({
+    path: "/api/updates/test",
+    method: "POST",
+    status: "SUCCESS"
+  });
+});
+
+export default router;
