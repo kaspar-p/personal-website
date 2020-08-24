b4890337ecbe80d7d49445e787164ce8b22cec69
Kaspar Poland <kaspar78@mouco.com>
2020-08-23 18:18:09 -0600

    Make circles divs with borderRadius rather than images

--- client/src/components/Circles.js
+++ client/src/components/Circles.js
@@ -1,37 +1,30 @@
 import React from "react";
-import circleImg from "../assets/images/blackCircle.png";
+import _ from "lodash";
 
 import "../assets/css/circles.css";
 
-function Squares(props) {
+function Circles(props) {
   const rate = 0.6;
   const offset = -12;
-  const fromLeftOffset = 920;
+  const fromLeftOffset = 930;
   const pixelsBetween = 20;
 
-  const squares = [];
-  for (let i = 0; i < 20; i++) {
-    squares.push(
-      <div
-        className="square"
-        key={i}
-        style={{
-          left: fromLeftOffset + i * pixelsBetween,
-          animationDelay: `${offset + rate * i}s`
-        }}
-      >
-        <img
-          className="customImg"
-          src={circleImg}
-          alt="Black Circle"
-          height="40"
-          width="40"
-        />
-      </div>
-    );
-  }
-
-  return <div className="squareWrapper">{squares}</div>;
+  return (
+    <div className="squareWrapper">
+      {_.times(20, i => (
+        <div
+          className="circle-wrapper"
+          key={i}
+          style={{
+            left: fromLeftOffset + i * pixelsBetween,
+            animationDelay: `${offset + rate * i}s`
+          }}
+        >
+          <div className="circle"></div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Squares;
+export default Circles;
